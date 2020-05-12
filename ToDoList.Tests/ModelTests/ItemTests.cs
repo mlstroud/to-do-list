2a74ed6098e8892702d6a6521c3d0bf5dc483b4d
Matt Stroud <lixingke06@gmail.com>
2020-05-11 22:35:20 -0700

    finish tests

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,11 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
 using ToDoList.Models;
 
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemTests
+  public class ItemTests : IDisposable
   {
+    public void Dispose()
+    {
+      Item.ClearAll();
+    }
 
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
@@ -36,6 +42,30 @@ namespace ToDoList.Tests
       string result = newItem.Description;
 
       Assert.AreEqual(updatedDescription, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+      List<Item> newList = new List<Item>();
+
+      List<Item> result = Item.GetAll();
+
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      string description1 = "Walk the dog.";
+      string description2 = "Wash the dishes.";
+      Item newItem1 = new Item(description1);
+      Item newItem2 = new Item(description2);
+      List<Item> newList = new List<Item> { newItem1, newItem2 };
+
+      List<Item> result = Item.GetAll();
+
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
